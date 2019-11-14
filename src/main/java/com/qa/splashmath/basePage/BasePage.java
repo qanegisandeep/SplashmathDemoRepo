e075e150fa8871dfaf2eb067b6ccfe7f753c4323
sandeepnegi <sandeep.negi@splashmth.com>
2019-11-14 17:14:53 +0530

    change in chromeDriver

--- src/main/java/com/qa/splashmath/basePage/BasePage.java
+++ src/main/java/com/qa/splashmath/basePage/BasePage.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import io.github.bonigarcia.wdm.OperatingSystem;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BasePage {
@@ -27,9 +28,14 @@ public class BasePage {
 	public WebDriver init_driver(String browserName) {
 		
 		if(browserName.equalsIgnoreCase("chrome")) {
-			WebDriverManager.chromedriver().setup();
+			//WebDriverManager.chromedriver().setup();
 			//driver = new ChromeDriver();
+			//tldriver.set(new ChromeDriver());
+	
+		// Added to run via jenkins	
+			WebDriverManager.chromedriver().operatingSystem(OperatingSystem.LINUX).setup();
 			tldriver.set(new ChromeDriver());
+			
 		}
 		else if(browserName.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
